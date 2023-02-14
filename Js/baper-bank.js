7f3f3c054f3b74cd5f987c0cdba90c8b54d2f30b
Progga <proggaroy2000@gmail.com>
2023-02-14 20:24:33 +0600

    login page complete and go to the another page

---
+++ Js/baper-bank.js
@@ -0,0 +1,19 @@
+document.getElementById('submit-btn').addEventListener('click',function(){
+    // for email
+const inputField = document.getElementById('input-field')
+const inputText = inputField.value;
+console.log(inputText)
+
+// for password
+const passwordField = document.getElementById('password-field')
+const passwordText = passwordField.value;
+console.log(passwordText)
+
+if(inputText === 'proggaroy2023@gmail.com' && passwordText === 'roy3312progga') {
+window.location.href ='bank.html'
+}
+else{
+    alert('WRONG!! try again with validation')
+}
+
+})
