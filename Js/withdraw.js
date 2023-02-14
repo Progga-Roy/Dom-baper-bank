e18fd66c1a70b6c607b59d6570f32e763e38bba1
Progga <proggaroy2000@gmail.com>
2023-02-15 00:37:06 +0600

    withdraw amount set

--- Js/withdraw.js
+++ Js/withdraw.js
@@ -1,1 +1,18 @@
-document.getElementById('')
+
+
+document.getElementById('withdraw-btn').addEventListener('click', function() {
+    const withdrawField = document.getElementById('withdraw-field');
+    const withdrawFieldText = withdrawField.value;
+    const withdrawFieldTextString = parseFloat(withdrawFieldText);
+  
+    const withdrawAmount = document.getElementById('withdraw-amount');
+    const withdrawAmountText = withdrawAmount.innerText;
+    const withdrawAmountTextString = parseFloat(withdrawAmountText);
+  
+    const countWithdrawBalance = withdrawAmountTextString + withdrawFieldTextString;
+    withdrawAmount.innerText = countWithdrawBalance;
+    withdrawField.value = '';
+  });
+  
+
+
